8fa0ad86eabe61ec76b97a5fca8fdd445bd63708
artainmo <tainmontarthur@icloud.com>
2023-03-04 19:45:53 +0100

    local gitlab script improved

--- bonus/scripts/local_gitlab.sh
+++ bonus/scripts/local_gitlab.sh
@@ -9,8 +9,15 @@ echo "Make sure you have correctly set up your DNS, and change https://gitlab.ex
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.deb.sh | sudo bash
 sudo EXTERNAL_URL="http://gitlab.iot.com" apt-get install gitlab-ee
 
-echo "Gitlab UI: $EXTERNAL_URL"
+sudo PASS=$(cat /etc/gitlab/initial_root_password)
+
+echo "Gitlab UI: http://gitlab.iot.com"
 echo "Login: root"
-echo "Password: $(cat /etc/gitlab/initial_root_password)"
+echo "Password: $PASS (we pasted it on clipboard)"
+if [ "$(uname)" = "Darwin" ]; then
+   echo $PASS | pbcopy
+else
+  echo $PASS | xsel --clipboard --input
+fi
 
-
+read -p "Enter to coninue..."
