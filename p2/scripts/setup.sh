6d07315e0534962f572ce46958f27a3f132a360b
Aglorios17 <alessio.glorioso17@gmail.com>
2023-03-04 15:02:03 +0100

    auto-correct p1/p2

--- p2/scripts/setup.sh
+++ p2/scripts/setup.sh
@@ -2,10 +2,10 @@
 
 curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE="644" INSTALL_K3S_EXEC="--flannel-iface eth1" sh -
 
-sleep 10
+sleep 20
 
-kubectl apply -f /vagrant/app1.yaml --validate=false
-kubectl apply -f /vagrant/app2.yaml --validate=false
-kubectl apply -f /vagrant/app3.yaml --validate=false
-kubectl apply -f /vagrant/ingress.yaml --validate=false
+kubectl apply -n kube-system -f /vagrant/app1.yaml --validate=false
+kubectl apply -n kube-system -f /vagrant/app2.yaml --validate=false
+kubectl apply -n kube-system -f /vagrant/app3.yaml --validate=false
+kubectl apply -n kube-system -f /vagrant/ingress.yaml --validate=false
 
