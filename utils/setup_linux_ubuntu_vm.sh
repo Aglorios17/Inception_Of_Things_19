47ae937dd4b9581a483defa4ddcc6abebb8d7704
artainmo <53705599+artainmo@users.noreply.github.com>
2023-03-05 02:24:22 +0100

    Update and rename setup.sh to setup_linux_ubuntu_vm.sh

--- utils/setup_linux_ubuntu_vm.sh
+++ utils/setup_linux_ubuntu_vm.sh
@@ -62,8 +62,3 @@ echo "\n>> ADD HOST IN /ETC/HOSTS\n"
 echo "192.168.56.110 app1.com" | sudo tee -a /etc/hosts
 echo "192.168.56.110 app2.com" | sudo tee -a /etc/hosts
 echo "127.0.0.1 gitlab.local" | sudo tee -a /etc/hosts
-
-
-
-
-
