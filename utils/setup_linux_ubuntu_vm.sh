828ceaa0c5c5fa4b737c925a0ee751c6e57a2dc9
Alessio Glorioso <56506324+Aglorios17@users.noreply.github.com>
2023-03-06 21:37:44 +0100

    Update setup_linux_ubuntu_vm.sh

--- utils/setup_linux_ubuntu_vm.sh
+++ utils/setup_linux_ubuntu_vm.sh
@@ -63,7 +63,7 @@ argocd version
 echo "\n>> INSTALL HELM\n"
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
-./get_helm.sh
+sudo ./get_helm.sh
 sudo rm get_helm.sh
 
 echo "\n>> ADD HOST IN /ETC/HOSTS\n"
